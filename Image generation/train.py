4cd7b9874ed2c69bfe66f37ceaf9dd71d991e619
ApplePieCZ <lukas1454@seznam.cz>
2024-04-18 15:10:14 +0200

    Create train.py
    Script for easier training.

---
+++ Image generation/train.py
@@ -0,0 +1,57 @@
+# KNN project - Diffusion model
+# Script for better training launching
+# Created by Lukas Marek
+# 18.4.2024
+
+from ddm import *
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Parser for training script")
+
+    parser.add_argument("path", type=str, help="Path to the data")
+    parser.add_argument("epochs", type=int, help="Final epoch for training")
+    parser.add_argument("name", type=str, help="Name of the training run")
+    parser.add_argument("--continue_training", nargs=2, metavar=("epoch", "checkpoint_file"),
+                        help="Continue training from a checkpoint")
+    parser.add_argument("--batch_size", type=int, help="Batch size for training", default=6)
+    parser.add_argument("--cuda", action="store_true", help="Enable CUDA for GPU acceleration", default=True)
+
+    return parser
+
+
+def check_model():
+    if not os.path.exists(checkpoint_file):
+        print(f"Checkpoint file {checkpoint_file} does not exist.")
+        exit(-1)
+    checkpoint = checkpoint_file.split("/")[-1]
+    ending = checkpoint.split(".")[-1]
+
+    if ending != "pt":
+        print(f"Checkpoint file {checkpoint_file} is in incorrect format.")
+        exit(-1)
+
+
+if __name__ == '__main__':
+    argument_parser = create_parser()
+    arguments = argument_parser.parse_args()
+
+    if arguments.continue_training:
+        continue_epoch, checkpoint_file = arguments.continue_training
+        check_model()
+        print(f"--- Continuing training from epoch {continue_epoch} with checkpoint file {checkpoint_file} ---")
+    else:
+        print("--- Starting fresh training ---")
+
+    if not os.path.exists(arguments.path):
+        print(f"Dataset folder {arguments.path} does not exist.")
+        exit(-1)
+
+    dataset = arguments.path.split("/")[-1]
+    print("Dataset:", dataset)
+    print("Epochs:", arguments.epochs)
+    print("Name:", arguments.name)
+    print("Batch size:", arguments.batch_size)
+    print("CUDA Enabled:", arguments.cuda)
+
+    launch(arguments)
